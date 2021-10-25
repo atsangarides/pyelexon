f1c26ae2c5c71375b6441a2ce32fcd918a030461
atsangarides <andreas_tsangarides@hotmail.com>
2021-10-25 13:58:46 +0100

    dict for renaming method PEP compliant argument names to the BMRS equivalent

---
+++ pyelexon/kwargs_mapping.py
@@ -0,0 +1,8 @@
+kwargs_mapping = {
+    "settlement_date": "SettlementDate",
+    "settlement_period": "SettlementPeriod",
+    "bm_unit_id": "BMUnitId",
+    "bm_unit_type": "BMUnitType",
+    "lead_party_name": "LeadPartyName",
+    "ngc_bm_unit_name": "NGCBMUnitName",
+}
